a028defe178e3070fbc17666ec969ae8ec976526
304089 <9457imai2@gmail.com>
2021-12-06 18:51:03 +0900

    gear index一部部分テンプレート化

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,6 +1,35 @@
 class PlansController < ApplicationController
+  before_action :set_user, except:[:new, :create, :index]
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
   def index
-    @plans = Plan.all.includes(:user)
+    @plans = Plan.where(user_id: current_user.id)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def plan_params
+    parmas.require(:plan).permit(:title, :departure_date, :return_date, :place, :check_in_time, :check_out_time, :memo, :member)
+  end
+
+  def set_plan
+    @plan = Plan.find(params[:id])
   end
 
   
